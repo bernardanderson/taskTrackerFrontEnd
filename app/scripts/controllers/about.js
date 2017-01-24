ec4c2820fc9af461bee3a1e5488747a344f667b0
Bernard Anderson <bernardanderson@gmail.com>
2017-01-23 19:20:39 -0600

    Initial Commit of FrontEnd

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name taskTrackerFrontEndApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the taskTrackerFrontEndApp
+ */
+angular.module('taskTrackerFrontEndApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name taskTrackerFrontEndApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the taskTrackerFrontEndApp
+ */
+angular.module('taskTrackerFrontEndApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
