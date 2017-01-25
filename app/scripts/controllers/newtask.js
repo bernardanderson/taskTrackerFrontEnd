2ae22e66602f705cad4bbe0c2d132ef10a7fa184
Bernard Anderson <bernard.anderson@gmail.com>
2017-01-24 23:16:52 -0600

    Added Post method to add new task

--- app/scripts/controllers/newtask.js
+++ app/scripts/controllers/newtask.js
@@ -8,11 +8,51 @@
  * Controller of the taskTrackerFrontEndApp
  */
 angular.module('taskTrackerFrontEndApp')
-  .controller('NewTaskCtrl', function ($scope) {
+  .controller('NewTaskCtrl', function ($scope, $http) {
 
-    $scope.printHello = function(){
-      console.log("I think this will print hello");
+    $scope.formData = {
+      TaskId: 0,
+      Name: "",
+      Description: "",
+      TaskStatus: 0,
     };
+
+    $scope.getFormData = function(){
+
+      if ($scope.formData.Name === "") {
+        $('div.field.name').addClass("error");
+      } else {
+        $('div.field.name').removeClass("error");
+      }
+
+      if ($scope.formData.Description === "") {
+        $('div.field.description').addClass("error");
+      } else {
+        $('div.field.description').removeClass("error");
+      }
+
+      if ($('div.field.description').hasClass("error") || $('div.field.name').hasClass("error")){
+        return;
+      }
+
+      $http({
+        method: 'POST',
+        url: 'http://localhost:5000/userTasks',
+        headers: { 'Content-Type': 'application/json' },
+        data: angular.toJson($scope.formData)
+        })
+        .then(data => console.log(data));
+    };
+
+    angular.element(document).ready(function () {
+      $('.ui.checkbox').checkbox().first().checkbox("set checked");
+      $('.ui.checkbox').checkbox({
+        onChecked: function() {
+          $scope.formData.TaskStatus = parseInt($(this).val());
+        }
+      });
+
+    });
 
 
   });
