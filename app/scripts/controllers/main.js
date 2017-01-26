0dc171a160a40fc3b778e143b0908028b964e34c
Bernard Anderson <bernard.anderson@gmail.com>
2017-01-26 00:06:49 -0600

    Added code to update a Task using contenteditable

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,6 +12,14 @@ angular.module('taskTrackerFrontEndApp')
     
     $scope.taskData = [];
 
+    $scope.editedTask = {}
+
+    $scope.editingMode = {
+      enabled: false,      
+      task: null,
+      hasFiredOnce: false
+    }
+
     $scope.convertTaskStatus = function(sentTaskStatusInt) {
       switch (sentTaskStatusInt) {
         case 0:
@@ -23,6 +31,80 @@ angular.module('taskTrackerFrontEndApp')
         default:
           return "N/A";
       }
+    }
+
+    $scope.fireDropDown = function(sentTask) {
+      if ($scope.editingMode.hasFiredOnce){
+        angular.element('.ui.dropdown').dropdown({
+          onChange: function(value, text, $selectedItem) {
+            for (let i = 0; i < $scope.taskData.length; i++) {
+              if ($scope.taskData[i].taskId === sentTask.taskId) {
+                $scope.taskData[i].taskStatus = parseInt(value);
+                angular.element(`.task_${sentTask.taskId}.dropd`).text($scope.convertTaskStatus(parseInt(value)));
+                return;
+              }
+            }
+          }
+        });
+      }
+    }
+
+    $scope.editItem = function(sentTask) {
+      $scope.editingMode.enabled = !$scope.editingMode.enabled;
+      $scope.editingMode.hasFiredOnce = true;
+
+      if ($scope.editingMode.enabled) {
+        $scope.editingMode.task = sentTask.taskId;
+        $scope.fireDropDown(sentTask);
+        angular.element(`.task_${sentTask.taskId}`).attr("contenteditable", "true");
+        angular.element(`.task_${sentTask.taskId}`).addClass("warning");
+      } else {
+        let name = angular.element(`.task_${sentTask.taskId}.name`);
+        let desc = angular.element(`.task_${sentTask.taskId}.desc`);
+        
+        if (name.text() === "") {
+          name.removeClass("warning")
+          name.addClass("error");
+          $scope.editingMode.enabled = true;
+        } else {
+          name.removeClass("error");
+        }
+
+        if (desc.text() === "") {
+          desc.removeClass("warning")
+          desc.addClass("error");
+          $scope.editingMode.enabled = true;
+        } else {
+          desc.removeClass("error");
+        }
+
+        if (name.text() !== "" && desc.text() !== "") {
+          $scope.editingMode.task = null;
+          angular.element(`.task_${sentTask.taskId}`).removeAttr("contenteditable");
+          angular.element(`.task_${sentTask.taskId}`).removeClass("warning");
+          $scope.editTask(sentTask);
+        }
+      }
+    }
+
+    $scope.editTask = function(sentTask) {
+      for (let i = 0; i < $scope.taskData.length; i++) {
+        if ($scope.taskData[i].taskId === sentTask.taskId) {
+          $scope.taskData[i].name = angular.element(`.task_${sentTask.taskId}.name`).text();
+          $scope.taskData[i].description = angular.element(`.task_${sentTask.taskId}.desc`).text();
+          $scope.putItem($scope.taskData[i]);
+          break;
+        }
+      }
+    }
+
+    $scope.putItem = function(sentTask) {
+      $http({
+        method: 'PUT',
+        url: `http://localhost:5000/userTasks/${sentTask.taskId}`,
+        headers: { 'Content-Type': 'application/json' },
+        data: angular.toJson(sentTask)
+        })
     }
 
     angular.element(document).ready(function () {
